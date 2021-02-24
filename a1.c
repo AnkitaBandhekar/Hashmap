69cf35cbf19a9a262dc84c0ea9a126e32b488691
AnkitaBandhekar <79565934+AnkitaBandhekar@users.noreply.github.com>
2021-02-24 14:27:39 +0530

    Delete a1.c

--- a1.c
+++
@@ -1,8 +0,0 @@
-#include <stdio.h>  
-main(){  
-   FILE *fp;  
-   fp = fopen("a1.txt", "w");//opening file  
-   fprintf(fp, "sbdxsadxbsjd...\n");//writing data into file  
-   fclose(fp);//closing file  
-}  
-
